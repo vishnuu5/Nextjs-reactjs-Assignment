1d3f67a389fdb9288c73f396b86eb200d3e95685
vishnuu5 <vishnuathmakuru20@gmail.com>
2024-02-26 13:20:30 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,244 @@
+import styles from "./index.module.css";
+
+const Desktop = () => {
+  return (
+    <div className={styles.desktop1}>
+      <div className={styles.rectangle} />
+      <div className={styles.todaysDeals}>Today's deals</div>
+      <div className={styles.websiteBuilders}>Website Builders</div>
+      <div className={styles.categories}>Categories</div>
+      <input type="text" className={styles.rectangle1} />
+      <img className={styles.imageIcon} alt="" src="/search.png" />
+      <div className={styles.bestWebsiteBuilders}>
+        Best Website builders in the US
+      </div>
+      <div className={styles.rectangle2} />
+      <div className={styles.rectangle3} />
+      <div className={styles.topRelevant}>Top Relevant</div>
+      <img className={styles.imageIcon1} alt="" src="/vectordown.png" />
+      <img className={styles.imageIcon2} alt="" src="/circle.png" />
+      <div className={styles.lastUpdated}>Last Updated</div>
+      <div className={styles.div}>-</div>
+      <div className={styles.february222020}>February 22, 2020</div>
+      <img className={styles.imageIcon3} alt="" src="/i.png" />
+      <div className={styles.advertisingDisclosure}>Advertising Disclosure</div>
+      <div className={styles.tools}>Tools</div>
+      <div className={styles.awsBuilder}>AWS Builder</div>
+      <div className={styles.startBuild}>Start Build</div>
+      <div className={styles.buildSupplies}>Build Supplies</div>
+      <div className={styles.tooling}>Tooling</div>
+      <div className={styles.bluehosting}>BlueHosting</div>
+      <div className={styles.home}>Home</div>
+      <img className={styles.vectorIcon} alt="" src="/vectorright.png" />
+      <div className={styles.hostingForAll}>Hosting for all</div>
+      <img className={styles.vectorIcon1} alt="" src="/vectorright.png" />
+      <div className={styles.hosting}>Hosting</div>
+      <img className={styles.vectorIcon2} alt="" src="/vectorright.png" />
+      <div className={styles.hosting6}>Hosting6</div>
+      <img className={styles.vectorIcon3} alt="" src="/vectorright.png" />
+      <div className={styles.hosting5}>Hosting5</div>
+      <div className={styles.rectangle4} />
+      <div className={styles.rectangle5} />
+      <div className={styles.div1}>1</div>
+      <img className={styles.rectangleIcon} alt="" src="/" />
+      <div className={styles.bestChoice}>Best Choice</div>
+      <img className={styles.imageIcon4} alt="" src="/cup.png" />
+      <img className={styles.download1Icon} alt="" src="/download-1@2x.png" />
+      <div className={styles.builder1}>Builder 1</div>
+      <div className={styles.wixpro72InchResponsiveContainer}>
+        <b>WixPro 72-Inch Responsive Website Builder</b>
+        <span>
+          - Comprehensive Digital Platform Creation Tool, Streamlined Design
+          Interface for Professional Websites and Online Stores (Black/Blue)
+        </span>
+      </div>
+      <div className={styles.rectangle6} />
+      <div className={styles.div2}>9.8</div>
+      <div className={styles.exceptional}>Exceptional</div>
+      <img className={styles.imageIcon5} alt="" src="/stars.png" />
+      <div className={styles.rectangle7} />
+      <div className={styles.view}>View</div>
+      <div className={styles.showMore}>Show more</div>
+      <div className={styles.whatYouGet}>
+        [What You Get]: Receive the WixPro website builder suite, access to
+        premium design templates, an extensive library of widgets and apps, and
+        detailed step-by-step guides.
+      </div>
+      <b className={styles.mainHighlights}>Main highlights</b>
+      <div className={styles.rectangle8} />
+      <div className={styles.rectangle9} />
+      <div className={styles.rectangle10} />
+      <div className={styles.bestValue}>Best Value</div>
+      <img className={styles.imageIcon6} alt="" src="/cup.png" />
+      <div className={styles.rectangle11} />
+      <div className={styles.div3}>2</div>
+      <img className={styles.download7Icon} alt="" src="/download-1@2x.png" />
+      <div className={styles.biulder}>Biulder</div>
+      <div className={styles.rectangle12} />
+      <div className={styles.div4}>9.5</div>
+      <div className={styles.excellent}>Excellent</div>
+      <img className={styles.imageIcon7} alt="" src="/stars.png" />
+      <div className={styles.view1}>
+        <p className={styles.view2}>View</p>
+      </div>
+      <div className={styles.sitecraft68InchUltimateContainer}>
+        <b>SiteCraft 68-Inch Ultimate Web Design Studio-</b>
+        <span>
+          {" "}
+          Advanced Site Creation Toolkit, Intuitive Drag-and-Drop Editor for
+          Dynamic Websites and E-commerce Platforms (Green/White)
+        </span>
+      </div>
+      <b className={styles.mainHighlights1}>Main highlights</b>
+      <div className={styles.whatYouGet1}>
+        [What You Get]: Gain access to the SiteCraft design studio, featuring a
+        robust selection of design elements, SEO optimization tools, and
+        e-commerce integrations.
+      </div>
+      <div className={styles.showMore1}>Show more</div>
+      <div className={styles.rectangle13} />
+      <div className={styles.rectangle14} />
+      <div className={styles.rectangle15} />
+      <div className={styles.div5}>3</div>
+      <img className={styles.download6Icon} alt="" src="/download-1@2x.png" />
+      <div className={styles.builder11}>Builder 1</div>
+      <div className={styles.rectangle16} />
+      <div className={styles.div6}>9.3</div>
+      <div className={styles.exceptional1}>Exceptional</div>
+      <img className={styles.icon} alt="" src="/stars.png" />
+      <div className={styles.rectangle17} />
+      <div className={styles.view3}>View</div>
+      <img className={styles.imageIcon8} alt="" src="/vectordown.png" />
+      <img className={styles.imageIcon9} alt="" src="/vectordown.png" />
+      <div className={styles.wixpro72InchResponsiveContainer1}>
+        <b>WixPro 72-Inch Responsive Website Builder</b>
+        <span>
+          - Comprehensive Digital Platform Creation Tool, Streamlined Design
+          Interface for Professional Websites and Online Stores (Black/Blue)
+        </span>
+      </div>
+      <b className={styles.mainHighlights2}>Main highlights</b>
+      <div className={styles.whatYouGet2}>
+        [What You Get]: Receive the WixPro website builder suite, access to
+        premium design templates, an extensive library of widgets and apps, and
+        detailed step-by-step guides.
+      </div>
+      <div className={styles.showMore2}>Show more</div>
+      <div className={styles.rectangle18} />
+      <div className={styles.div7}>4</div>
+      <img className={styles.download2Icon} alt="" src="/download-1@2x.png" />
+      <div className={styles.cdk}>CDK</div>
+      <div className={styles.rectangle19} />
+      <div className={styles.rectangle20} />
+      <div className={styles.div8}>9.1</div>
+      <img className={styles.imageIcon10} alt="" src="/stars.png" />
+      <div className={styles.veryGood}>Very Good</div>
+      <div className={styles.view4}>View</div>
+      <div className={styles.cdkResposiveBuilderContainer}>
+        <b>CDK Resposive Builder:</b>
+        <span>
+          {" "}
+          An extensive library of widgets and apps, and detailed step-by-step
+          guides
+        </span>
+      </div>
+      <div className={styles.rectangle21} />
+      <div className={styles.off}>26% Off</div>
+      <b className={styles.mainHighlights3}>Main highlights</b>
+      <div className={styles.rectangle22} />
+      <img className={styles.rectangleIcon1} alt="" src="/Rectangle.png" />
+      <img className={styles.rectangleIcon2} alt="" src="/Rectangle.png" />
+      <div className={styles.rectangle23} />
+      <div className={styles.div9}>9.9</div>
+      <div className={styles.div10}>8.9</div>
+      <div className={styles.div11}>8.9</div>
+      <div className={styles.buildingResponsive}>building responsive</div>
+      <div className={styles.cool}>Cool</div>
+      <div className={styles.docs}>Docs</div>
+      <div className={styles.whyWeLove}>Why we love it</div>
+      <img className={styles.imageIcon11} alt="" src="/tick.png" />
+      <img className={styles.imageIcon12} alt="" src="/tick.png" />
+      <img className={styles.imageIcon13} alt="" src="/tick.png" />
+      <div className={styles.showMore3}>Show more</div>
+      <div className={styles.documentation}>Documentation</div>
+      <div className={styles.easyUse}>Easy Use</div>
+      <div className={styles.outOfBox}>Out of box</div>
+      <div className={styles.relatedDealsYou}>
+        Related deals you might like for
+      </div>
+      <div className={styles.rectangle24} />
+      <img className={styles.download3Icon} alt="" src="/download-3@2x.png" />
+      <div className={styles.rectangle25} />
+      <div className={styles.rectangle26} />
+      <div className={styles.off1}>20% Off</div>
+      <div className={styles.limitedTime}>{`Limited time `}</div>
+      <b className={styles.webbuilder1}>Webbuilder 1</b>
+      <div className={styles.computerModernClasic}>
+        Computer Modern clasic with wix support
+      </div>
+      <div className={styles.div12}>$39.96</div>
+      <div className={styles.div13}>$49.96</div>
+      <div className={styles.off2}>(20% Off)</div>
+      <div className={styles.rectangle27} />
+      <div className={styles.viewDeal}>View Deal</div>
+      <div className={styles.rectangle28} />
+      <div className={styles.rectangle29} />
+      <img className={styles.download4Icon} alt="" src="/download-3@2x.png" />
+      <img className={styles.download5Icon} alt="" src="/download-3@2x.png" />
+      <div className={styles.rectangle30} />
+      <div className={styles.rectangle31} />
+      <div className={styles.off3}>20% Off</div>
+      <div className={styles.limitedTime1}>{`Limited time `}</div>
+      <img className={styles.rectangleIcon3} alt="" src="/rectangle.svg" />
+      <div className={styles.rectangle32} />
+      <div className={styles.off4}>20% Off</div>
+      <div className={styles.limitedTime2}>{`Limited time `}</div>
+      <b className={styles.webbuilder11}>Webbuilder 1</b>
+      <b className={styles.webbuilder12}>Webbuilder 1</b>
+      <div className={styles.computerModernClasic1}>
+        Computer Modern clasic with wix support
+      </div>
+      <div className={styles.div14}>$39.96</div>
+      <div className={styles.div15}>$49.96</div>
+      <div className={styles.off5}>(20% Off)</div>
+      <div className={styles.rectangle33} />
+      <div className={styles.viewDeal1}>View Deal</div>
+      <div className={styles.computerModernClasic2}>
+        Computer Modern clasic with wix support
+      </div>
+      <div className={styles.div16}>$39.96</div>
+      <div className={styles.div17}>$49.96</div>
+      <div className={styles.off6}>(20% Off)</div>
+      <div className={styles.rectangle34} />
+      <div className={styles.viewDeal2}>View Deal</div>
+      <div className={styles.signUpAnd}>
+        Sign up and get exclusive special deals
+      </div>
+      <input type="text" className={styles.rectangle35} />
+      <div className={styles.rectangle36} />
+      <div className={styles.signUp}>Sign Up</div>
+      <div className={styles.rectangle37} />
+      <div className={styles.categories1}>Categories</div>
+      <div className={styles.webBuilder}>Web Builder</div>
+      <div className={styles.hosting1}>Hosting</div>
+      <div className={styles.dataCenter}>Data Center</div>
+      <div className={styles.roboticAutomation}>Robotic-Automation</div>
+      <div className={styles.contact}>Contact</div>
+      <div className={styles.contact1}>Contact</div>
+      <div className={styles.privacyPolicy}>Privacy Policy</div>
+      <div className={styles.termsOfService}>Terms Of Service</div>
+      <div className={styles.categories2}>Categories</div>
+      <div className={styles.about}>About</div>
+      <div className={styles.unitedStates}>United States</div>
+      <img className={styles.imageIcon14} alt="" src="/vectordown.png" />
+      <div className={styles.rectangle38} />
+      <div className={styles.rectangle39} />
+      <div className={styles.rectangle40} />
+      <img className={styles.rectangleIcon4} alt="" src="/rectangle.svg" />
+      <div className={styles.rectangle41} />
+      <div className={styles.rectangle42} />
+    </div>
+  );
+};
+
+export default Desktop;
